8902a0c40e00e73660c009fa0f524f7d82996e13
NishanthAlmeida <nishanth.almeida@gmail.com>
2019-05-07 17:41:18 +0530

    2 files modified

--- component/segment-menu.tsx
+++ component/segment-menu.tsx
@@ -6,10 +6,22 @@ export interface segmentMenuProps{
   farItems: ISegmentMenuItems[];
 }
 
-export class SegmentMenu extends React.Component<segmentMenuProps> {
+interface segmentMenuState{
+  expandMenu: boolean;
+}
+
+export class SegmentMenu extends React.Component<segmentMenuProps, segmentMenuState> {
   constructor(props: segmentMenuProps) {
     super(props);
+    this.state = { expandMenu : true};
   }
+
+  expandMenu = (e) => {
+    debugger;
+    this.setState({expandMenu : (this.state.expandMenu) ? false : true});
+  }
+
+
   render() {
     console.log(this.props);
     return (
@@ -21,7 +33,16 @@ export class SegmentMenu extends React.Component<segmentMenuProps> {
           </span>
         )
       })}
-        <span>...</span>
+      <span className="menus" onClick={this.expandMenu}>
+        <div className="menu-item dot-icon"></div>
+      </span>
+      {this.props.farItems.map((r) => {
+        return (
+          <span className="menus" title={r.name} onClick={r.onClick}>
+            <div className={`menu-item ${r.icon}`}></div>
+          </span>
+        )
+      })}
       </div>
     );
   }
