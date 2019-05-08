2fee5c08223d45bf2478ff1ce480abba8224451e
NishanthAlmeida <nishanth.almeida@gmail.com>
2019-05-08 19:06:24 +0530

    1 file modified

--- data/mock-data.ts
+++ data/mock-data.ts
@@ -143,6 +143,42 @@ export default {
         { "metaDatalabel": "RequestedBy", "metaDataText": "Jane Doe" },
         { "metaDatalabel": "RequestedDate", "metaDataText": "12/03/2019" }
       ]
+    },
+    {
+      "applicationName": "Finance Manage Approval1",
+      "processName": "Finance Process",
+      "priority": "High",
+      "timeDetails": {
+        "processtarted": "3 Days Ago",
+        "dueDays": "Since 5 Days"
+      },
+      "metaDataDetails": [
+        { "metaDatalabel": "Region", "metaDataText": "EMEA" },
+        { "metaDatalabel": "Amount", "metaDataText": "1400" },
+        { "metaDatalabel": "Department", "metaDataText": "Procurement" },
+        { "metaDatalabel": "Vendor", "metaDataText": "AgilePoint INC" },
+        { "metaDatalabel": "Product", "metaDataText": "Agile Point NX 7.0" },
+        { "metaDatalabel": "RequestedBy", "metaDataText": "Jane Doe" },
+        { "metaDatalabel": "RequestedDate", "metaDataText": "12/03/2019" }
+      ]
+    },
+    {
+      "applicationName": "Finance Manage Approval1",
+      "processName": "Finance Process",
+      "priority": "High",
+      "timeDetails": {
+        "processtarted": "3 Days Ago",
+        "dueDays": "Since 5 Days"
+      },
+      "metaDataDetails": [
+        { "metaDatalabel": "Region", "metaDataText": "EMEA" },
+        { "metaDatalabel": "Amount", "metaDataText": "1400" },
+        { "metaDatalabel": "Department", "metaDataText": "Procurement" },
+        { "metaDatalabel": "Vendor", "metaDataText": "AgilePoint INC" },
+        { "metaDatalabel": "Product", "metaDataText": "Agile Point NX 7.0" },
+        { "metaDatalabel": "RequestedBy", "metaDataText": "Jane Doe" },
+        { "metaDatalabel": "RequestedDate", "metaDataText": "12/03/2019" }
+      ]
     }
   ]
 }
