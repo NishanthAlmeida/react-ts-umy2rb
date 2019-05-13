f5170af4fda0662725788127bae53fc3ace95cb6
NishanthAlmeida <nishanth.almeida@gmail.com>
2019-05-13 16:47:18 +0530

    4 files modified

--- component/taskdetails-component.tsx
+++ component/taskdetails-component.tsx
@@ -2,18 +2,22 @@ import * as React from 'react';
 import { default as TaskItems } from '../data/mock-data';
 import { TaskCard } from '../component/task-cards';
 import '../css/mytasks.css';
+import { default as MyTaskDefaults } from '../defaults/taskcard-defaults';
 
 function getFilteredTasks(status: string) {
   return TaskItems.HomePageData.filter((item) => (item.status === status));
 }
 
 export const statusContainer = (status: string, total: number) => {
-  return (<div>{status}: {total}</div>);
+  return (
+    <div className="status-heading">
+      <span>{status} </span>
+      <span style={{color: MyTaskDefaults.MyTaskDefaults.StatusColorCodes[status]}}>{total}</span>
+    </div>);
 }
 
 export class TaskDetails extends React.Component<any, any>{
   render() {
-    debugger;
     const overdueTasks = getFilteredTasks('Overdue'), overdueTaskTotal = overdueTasks.length;
     const assignedTasks = getFilteredTasks('Assigned'), assignedTaskTotal = assignedTasks.length;
     const newTasks = getFilteredTasks('New'), newTaskTotal = overdueTasks.length;
@@ -34,7 +38,7 @@ export class TaskDetails extends React.Component<any, any>{
         <div className="taskdetails-container">
           {statusContainer('New', newTaskTotal)}
           {getFilteredTasks('New').map((r) => {
-            return (<TaskCard taskDetails={r}/>);
+            return (<TaskCard taskDetails={r} />);
           })}
         </div>
       </div>
--- defaults/taskcard-defaults.tsx
+++ defaults/taskcard-defaults.tsx
@@ -1,6 +1,13 @@
 
 
 export default {
+  MyTaskDefaults:{
+    StatusColorCodes: {
+      Assigned: '#1EA0E9',
+      New: '#95D579',
+      Overdue: '#EEC5C5'
+    }
+  },
   TaskCardDefaultValues : {
     cardExpandedHeight: '400px',
     cardHeight: '220px'
