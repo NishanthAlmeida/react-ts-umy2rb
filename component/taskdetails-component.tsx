ccae9f5d1e0f0711d603dd3516f7d1ce68641996
NishanthAlmeida <nishanth.almeida@gmail.com>
2019-05-13 16:34:15 +0530

    1 file modified

--- component/taskdetails-component.tsx
+++ component/taskdetails-component.tsx
@@ -4,31 +4,35 @@ import { TaskCard } from '../component/task-cards';
 import '../css/mytasks.css';
 
 function getFilteredTasks(status: string) {
-  return TaskItems.HomePageData.filter((item) => { return { asdasd : (item.status === status)} });
+  return TaskItems.HomePageData.filter((item) => (item.status === status));
 }
 
-export const statusContainer = (status: string) => {
-  return (<div>{status}</div>);
+export const statusContainer = (status: string, total: number) => {
+  return (<div>{status}: {total}</div>);
 }
 
 export class TaskDetails extends React.Component<any, any>{
   render() {
+    debugger;
+    const overdueTasks = getFilteredTasks('Overdue'), overdueTaskTotal = overdueTasks.length;
+    const assignedTasks = getFilteredTasks('Assigned'), assignedTaskTotal = assignedTasks.length;
+    const newTasks = getFilteredTasks('New'), newTaskTotal = overdueTasks.length;
     return (
       <div className="mytasks-container">
         <div className="taskdetails-container">
-          {statusContainer('Overdue')}
-          {getFilteredTasks('Overdue').map((r) => {
+          {statusContainer('Overdue', overdueTaskTotal)}
+          {overdueTasks.map((r) => {
             return (<TaskCard taskDetails={r} />);
           })}
         </div>
         <div className="taskdetails-container">
-          {statusContainer('Assigned')}
+          {statusContainer('Assigned', assignedTaskTotal)}
           {getFilteredTasks('Assigned').map((r) => {
             return (<TaskCard taskDetails={r} />);
           })}
         </div>
         <div className="taskdetails-container">
-          {statusContainer('New')}
+          {statusContainer('New', newTaskTotal)}
           {getFilteredTasks('New').map((r) => {
             return (<TaskCard taskDetails={r}/>);
           })}
